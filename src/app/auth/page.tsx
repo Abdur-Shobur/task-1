e51ffb27f05c35b1f54b3af0533fb44e5de2ff6d
abdur-shobur <abdur.shobur.me@gmail.com>
2025-11-29 18:58:52 +0600

    Feat: Add MetaData

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import PageClient from './page.client';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+  description:
+    'Sign in to your account or create a new one to start chatting with Monica',
+};
 
 const Page = () => {
   return (
--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
 import PageClient from './pageClient';
+
+export const metadata: Metadata = {
+  title: 'Chat with Monica',
+  description: 'Ask Monica about lifestyle, wellbeing, or legal support',
+};
 
 const Page = () => {
   return <PageClient />;
